f5019b170a32b81b6fa6f090c12e3ace5b6fcdf2
Daniel Kurz <danielkurz03@protonmail.com>
2022-05-19 19:20:50 +0200

    add flow helper

---
+++ tests/flow.test.ts
@@ -0,0 +1,21 @@
+import { delay } from "../src/flow";
+
+describe("time", () => {
+    it("should take 1 second", async () => {
+        const t1 = Date.now();
+        await delay(1000);
+        const t2 = Date.now()
+
+        expect(t2 - t1).toBeGreaterThanOrEqual(1000)
+        expect(t2 - t1).toBeLessThanOrEqual(1010)
+    })
+
+    it("should take 1.5 second", async () => {
+        const t1 = Date.now();
+        await delay(1500);
+        const t2 = Date.now()
+
+        expect(t2 - t1).toBeGreaterThanOrEqual(1500)
+        expect(t2 - t1).toBeLessThanOrEqual(1510)
+    })
+})
